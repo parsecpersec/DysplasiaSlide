197d2b188d3d8462062b5cf02dff35c86ae532bd
parsecpersec <53437339+parsecpersec@users.noreply.github.com>
2021-10-22 20:39:08 +0800

    Update run patch.R

--- 3. score calculation/run patch.R
+++ 3. score calculation/run patch.R
@@ -1,6 +1,6 @@
 setwd('')
 
-#### generate patch file ####
+#### generate patch file - large ####
 files = list.files('./soft_patch/temp_large/')
 path = unique(substr(files, 1, 9))
 soft = unique(substr(files, 11, length(files)))
@@ -31,6 +31,38 @@ dat = dat[dat$path %in% lab$path,]
 dat$dys = lab$dys_bdkq[match(dat$path, lab$path)]
 write.csv(dat, './soft_patch/large.csv', quote=F, row.names=T)
 
+#### generate patch file - small ####
+files = list.files('./soft_patch/temp_small/')
+path = unique(substr(files, 1, 9))
+soft = unique(substr(files, 11, length(files)))
+soft = gsub('[.]csv', '', soft)
+
+dat = data.frame()
+dat0 = data.frame()
+for(p in path) {
+  for(s in soft) {
+    filename = paste0('./soft_patch/temp_small/', p, '_', s, '.csv')
+    temp = read.csv(filename, header=F, stringsAsFactors=F, row.names='V1')
+    if(s == soft[1]) {
+      rownames(temp) = paste0(p, '_', rownames(temp))
+      dat0 = temp
+    } else {
+      rownames(temp) = paste0(p, '_', rownames(temp))
+      dat0 = cbind(dat0, temp)
+    }
+  }
+  colnames(dat0) = soft
+  dat = rbind(dat, dat0)
+}
+
+dat$path = substr(rownames(dat), 1, 9)
+lab = read.csv('./saved.csv', T, stringsAsFactors=F)
+lab = lab[lab$dys_bdkq != 'Cancer',]
+dat = dat[dat$path %in% lab$path,]
+dat$dys = lab$dys_bdkq[match(dat$path, lab$path)]
+write.csv(dat, './soft_patch/small.csv', quote=F, row.names=T)
+
+#### density plot ####
 library(ggplot2)
 plot_dens = function(index) {
   tiff(paste0('./density_patch/', soft[index], '.tiff'), width=1800, height=1200, res=300)
@@ -43,3 +75,10 @@ plot_dens = function(index) {
 for(i in 1:length(soft)) {
   plot_dens(i)
 }
+
+#### try scores ####
+
+#### 1. mean ####
+# run score.R #
+
+#### 2. ####
