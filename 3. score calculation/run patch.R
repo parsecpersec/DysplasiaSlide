d6bca61bbad2db3db721d4b9dd7ef44c128e5a62
parsecpersec <53437339+parsecpersec@users.noreply.github.com>
2021-10-24 15:59:24 +0800

    add 2

--- 3. score calculation/run patch.R
+++ 3. score calculation/run patch.R
@@ -224,4 +224,121 @@ ggplot(dat, aes(x=score, fill=dys_bdkq, color=dys_bdkq)) + geom_density(size=1, 
         plot.title=element_text(hjust=0.5), title = element_text(size = 18, hjust=0.5)) +
   ggtitle(name) + xlab('Score') + ylab('Density')
 
-#### 2. ####
+#### 2. CV ####
+name = 'CV'
+dat = read.csv('./saved.csv', T, stringsAsFactors=F)
+dat = dat[dat$dys_bdkq != 'Cancer', ]
+cv <- function(x) {return(sd(x)/mean(x))}
+for(f in filename) {
+  if(grepl('large', f)) {
+    temp = aggregate(soft1[, f], FUN=cv, by=list(path=soft1$path))
+    dat = cbind(dat, temp$x)
+  } else {
+    temp = aggregate(soft2[, f], FUN=cv, by=list(path=soft2$path))
+    dat = cbind(dat, temp$x)
+  }
+}
+
+colnames(dat)[series] = filename
+for(i in series) {
+  dat[,i] = 100*dat[,i]    # (0-1) -> (0-100)
+}
+dat$dys_bdkq = factor(dat$dys_bdkq, levels=c("Hyperplasia",
+                                             "Mild dysplasia",
+                                             "Moderate dysplasia",
+                                             "Severe dysplasia"))
+
+# OR assess
+OR = data.frame()
+for (i in 1:length(filename)) {
+  fit = polr(dat$dys_bdkq ~ dat[,(i+8)], Hess=T)
+  res = exp(cbind(OR=coef(fit), confint.lm(fit, level=0.95)[1,]))
+  res = as.data.frame(res, stringsAsFactors=F)
+  res$var = c(paste0('lower_', i), paste0('upper_', i))
+  OR = rbind(OR, res)
+}
+
+OR$var = 1
+OR$var[seq(1, 2*length(files), 2)] = OR$V2[seq(2, 2*length(files), 2)]
+OR = OR[-seq(2, 2*length(files), 2),]
+rownames(OR) = 1:length(files)
+colnames(OR) = c('OR', 'lower', 'upper')
+OR$include = ifelse((OR$lower-1)*(OR$upper-1)>0, T, F)
+OR$var = colnames(dat)[series]
+
+# OR sorted by size
+OR2 = plyr::arrange(OR, var, decreasing=F)
+OR2$var = factor(OR2$var, levels=rev(OR2$var))
+# OR2[,1:3] = log(OR2[,1:3])  # if OR too large
+
+pd = position_dodge(0.1)
+ggplot(OR2, aes(x=var, y=OR, color=include)) + coord_flip() +
+  ggtitle(name) + 
+  geom_point(position=pd, size=3, shape=15) + 
+  geom_errorbar(aes(ymin=lower, ymax=upper), width=0.4, position=pd, size=1.2) +
+  geom_line(position=pd, size = 0.75) + xlab('') + ylab('Odds Ratio') +
+  geom_hline(yintercept = 1, linetype = 'dashed', color = "blue", size = 1) + theme_bw() +
+  theme(panel.background=element_blank()) +
+  theme(axis.text.x = element_text(size = 15), axis.text.y = element_text(size = 15),
+        title = element_text(size = 18, hjust=0.5), legend.position='none',
+        plot.title=element_text(hjust=0.5)) +
+  scale_color_manual(values=c('grey', 'tomato'))
+
+# calculate score
+sum = rep(0, nrow(dat))
+n = 0
+for (i in 1:length(filename)) {
+  if (T) {
+    sum = sum + dat[(i+8)] *  OR$OR[(i)]
+    n = n + 1
+  }
+}
+dat$score = sum[,1] / n
+
+fit = polr(dys_bdkq~score, Hess=T, data=dat)
+summary(fit)
+res = exp(cbind(OR=coef(fit), confint(fit)))
+res = as.data.frame(res, stringsAsFactors=F)
+res$var = rownames(res)
+
+# Score
+fit2 = polr(dys_bdkq~score+age+sex+smoke+drink, Hess=T, data=dat)
+summary(fit2)
+res2 = exp(cbind(OR=coef(fit2), confint(fit2)))
+res2 = as.data.frame(res2, stringsAsFactors=F)
+res2$var = rownames(res2)
+
+colnames(res2) = c('OR', 'lower', 'upper', 'var')
+res2$var = c('')
+res2$var = factor(res2$var, levels=res2$var)
+res2$mark = c(T, F, F, F, F)
+pd = position_dodge(0.1)
+ggplot(res2, aes(x=var, y=OR, color=mark)) + geom_point(position=pd, size=3, shape=15) + 
+  geom_errorbar(aes(ymin=lower, ymax=upper), width=0.4, position=pd, size=1.2) +
+  ggtitle(name) + xlab('') + ylab('Odds Ratio (OR)') +
+  theme_bw() + theme(panel.background=element_blank(), legend.position='none',
+                     axis.text.x = element_text(size = 15), axis.text.y = element_text(size = 15),
+                     plot.title=element_text(hjust=0.5),
+                     title = element_text(size = 18, hjust=0.5)) +
+  geom_hline(yintercept = 1, linetype = 'dashed', color = "blue", size = 1) + 
+  scale_color_manual(values=c('black', 'tomato'))
+
+# extra
+label = levels(factor(dat$dys_bdkq))
+comps = combn(label, 2)
+mycomp = list(comps[,1], comps[,2], comps[,3], comps[,4], comps[,5], comps[,6])
+ggplot(dat, aes(x=dys_bdkq, y=score, fill=dys_bdkq)) + geom_boxplot() + 
+  stat_compare_means(method='anova', label.y=85) +
+  stat_compare_means(method='kruskal', label.x=2, label.y=85) + 
+  stat_compare_means(comparisons=mycomp, method='wilcox') + theme_bw() +
+  theme(panel.background=element_blank(), legend.position='none',
+        axis.text.x = element_text(size = 15), axis.text.y = element_text(size = 15),
+        plot.title=element_text(hjust=0.5), title = element_text(size = 18, hjust=0.5)) +
+  ggtitle(name) + xlab('') + ylab('Score')
+
+ggplot(dat, aes(x=score, fill=dys_bdkq, color=dys_bdkq)) + geom_density(size=1, alpha=0.1) + 
+  theme_bw() + xlim(c(0 * min(dat$score), 1.25 * max(dat$score))) +
+  theme(panel.background=element_blank(), 
+        axis.text.x = element_text(size = 15), axis.text.y = element_text(size = 15),
+        plot.title=element_text(hjust=0.5), title = element_text(size = 18, hjust=0.5)) +
+  ggtitle(name) + xlab('Score') + ylab('Density')
