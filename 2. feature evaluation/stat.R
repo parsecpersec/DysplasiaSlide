f031ef19789e083263a547a852190a8049d33f4b
parsecpersec <53437339+parsecpersec@users.noreply.github.com>
2021-10-29 19:21:53 +0800

    Update stat.R

--- 2. feature evaluation/stat.R
+++ 2. feature evaluation/stat.R
@@ -11,17 +11,43 @@ library(ggplot2)
 library(reshape2)
 for(i in 1:16) {
   temp = dat[dat[,(4+i)] == 'YY',]
-  if(length(unique(temp$size)) == 2) {
-    tiff(paste0('./barplot of positive patches/', name[i], '.tiff'), res=300, height=1200, width=1800)
-    print(ggplot(temp, aes(x=dysplasia, group=size)) + 
-            geom_bar(aes(fill=factor(..x..)), stat='count', color='black') +
-            theme_bw() + theme(panel.background=element_blank(), legend.position='none',
-                               axis.text=element_text(size=15), title=element_text(size=18),
-                               plot.title=element_text(size=20, hjust=0.5, family='SimHei'),
-                               axis.text.x=element_text(size=13)) +
-            geom_text(aes(label = scales::percent(2*..count../sum(..count..))), 
-                      stat= "count", vjust = -0.25) +
-            ggtitle(name[i]) + xlab('') + ylab('Quantity') + facet_grid(~size))
-    dev.off()
+  if(nrow(temp) != 0) {
+    if(length(unique(temp$size)) == 2) {
+      tiff(paste0('./barplot of positive patches/', name[i], '.tiff'), res=300, height=1200, width=1800)
+      print(ggplot(temp, aes(x=dysplasia, group=size)) + 
+              geom_bar(aes(fill=factor(..x..)), stat='count', color='black') +
+              theme_bw() + theme(panel.background=element_blank(), legend.position='none',
+                                 axis.text=element_text(size=15), title=element_text(size=18),
+                                 plot.title=element_text(size=20, hjust=0.5, family='SimHei'),
+                                 axis.text.x=element_text(size=13)) +
+              geom_text(aes(label = scales::percent(2*..count../sum(..count..))), 
+                        stat= "count", vjust = -0.25) +
+              ggtitle(name[i]) + xlab('') + ylab('Quantity') + facet_grid(~size))
+      dev.off()
+    } else if(unique(temp$size) == 'large') {
+      tiff(paste0('./barplot of positive patches/', name[i], '.tiff'), res=300, height=1200, width=1800)
+      print(ggplot(temp, aes(x=dysplasia)) + 
+              geom_bar(aes(fill=factor(..x..)), stat='count', color='black') +
+              theme_bw() + theme(panel.background=element_blank(), legend.position='none',
+                                 axis.text=element_text(size=15), title=element_text(size=18),
+                                 plot.title=element_text(size=20, hjust=0.5, family='SimHei'),
+                                 axis.text.x=element_text(size=13)) +
+              geom_text(aes(label = scales::percent(..count../sum(..count..))), 
+                        stat= "count", vjust = -0.25) +
+              ggtitle(paste0(name[i], ' - large')) + xlab('') + ylab('Quantity'))
+      dev.off()
+    } else if(unique(temp$size) == 'small') {
+      tiff(paste0('./barplot of positive patches/', name[i], '.tiff'), res=300, height=1200, width=1800)
+      print(ggplot(temp, aes(x=dysplasia)) + 
+              geom_bar(aes(fill=factor(..x..)), stat='count', color='black') +
+              theme_bw() + theme(panel.background=element_blank(), legend.position='none',
+                                 axis.text=element_text(size=15), title=element_text(size=18),
+                                 plot.title=element_text(size=20, hjust=0.5, family='SimHei'),
+                                 axis.text.x=element_text(size=13)) +
+              geom_text(aes(label = scales::percent(..count../sum(..count..))), 
+                        stat= "count", vjust = -0.25) +
+              ggtitle(paste0(name[i], ' - small')) + xlab('') + ylab('Quantity'))
+      dev.off()
+    }
   }
 }
