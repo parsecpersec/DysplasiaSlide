b132951dfe7b94b4533e4fc6284cf945f2fef97a
parsecpersec <53437339+parsecpersec@users.noreply.github.com>
2021-11-17 14:32:37 +0800

    Update and rename Concordance.R to concordance.R

--- 2. feature evaluation/concordance.R
+++ 2. feature evaluation/concordance.R
@@ -5,6 +5,32 @@ library(ggplot2)
 library(irr)
 library(extrafont)
 
+#### record ####
+for (i in 2:17) {
+  dat[,i][dat1[,i] == 'Yes' & dat2[,i] == 'Yes' & dat3[,i] == 'Yes'] = 'Yes'
+  dat[,i][!(dat1[,i] == 'Yes' & dat2[,i] == 'Yes' & dat3[,i] == 'Yes')] = 'No'
+}
+
+kc = data.frame()
+for (i in 2:17) {
+  kc = rbind(kc, 
+             data.frame(feature=i-1,
+                        kappa=kappam.fleiss(data.frame(P1=dat1[,i], P2=dat2[,i], P3=dat3[,i]))$value))
+}
+write.csv(kc, 'kappa_C.csv', quote=F, row.names=F)
+
+for (i in 2:17) {
+  dat[,i][dat1[,i] == 'Yes' & dat2[,i] == 'Yes' & dat3[,i] == 'Yes'] = 'Yes'
+  dat[,i][!(dat1[,i] == 'Yes' & dat2[,i] == 'Yes' & dat3[,i] == 'Yes')] = 'No'
+}
+
+kd = data.frame()
+for (i in 2:17) {
+  kd = rbind(kd, 
+             data.frame(feature=i-1,
+                        kappa=kappam.fleiss(data.frame(P1=dat1[,i], P2=dat2[,i], P3=dat3[,i]))$value))
+}
+write.csv(kd, 'kappa_D.csv', quote=F, row.names=F)
 
 #### C+D 3 ####
 
