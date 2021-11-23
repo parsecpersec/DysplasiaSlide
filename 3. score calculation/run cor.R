e50b8fd960382f3f9c55feb4bd67d52be2d69560
parsecpersec <53437339+parsecpersec@users.noreply.github.com>
2021-11-23 14:01:52 +0800

    Update run cor.R

--- 3. score calculation/run cor.R
+++ 3. score calculation/run cor.R
@@ -1,8 +1,9 @@
 setwd('')
-# library(ggplot2)
 # library(GGally)
 # library(corrgram)
 library(corrplot)
+library(ggplot2)
+library(factoextra)
 
 #### start ####
 
@@ -29,6 +30,24 @@ for(i in series) {
 corrplot(cor(as.matrix(dat[,series]), method='pearson'), method='square', order='hclust',
          col=colorRampPalette(c('blue', 'white', 'tomato'))(200), mar=c(0, 0, 1, 0),
          tl.col='black', tl.srt=45, tl.cex=1.5, cl.cex=1.5)
+
+#### slide PCA ####
+slide.pca = prcomp(dat[,series], center=T, scale.=T)
+summary(slide.pca)
+scree = data.frame(pc=1:length(slide.pca$sdev), Variances=slide.pca$sdev)
+ggplot(scree, aes(x=pc, y=Variances)) + geom_line(stat='identity', size=1.2) + 
+  geom_point(size=3.5, color='blue') + ggtitle('Screeplot of the Principal Components') +
+  scale_x_continuous(breaks=1:12) + theme_bw() + xlab('') +
+  geom_hline(yintercept=1, color='red', linetype=2, size=1) +
+  theme(plot.title=element_text(size=20, hjust=0.5), axis.title=element_text(size=18),
+        axis.text=element_text(size=14))
+
+dat$dysplsia = ifelse(dat$dys_bdkq == 'Hyperplasia', 'Nondysplasia', 'Dysplasia')
+fviz_pca_ind(slide.pca, geom.ind='point', pointshape=21, pointsize=2, 
+             fill.ind=dat$dysplsia, col.ind='black', palette='jco', 
+             addEllipses=T, label='var', col.var='black', repel=T, legend.title='Class') +
+  ggtitle("2D PCA-plot from 12-feature dataset") + theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5))
 
 #### patch level ####
 # rm(list=ls())
